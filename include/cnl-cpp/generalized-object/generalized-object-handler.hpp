6b01eed295c88b34c52cc184020d6e0906e9a1d7
Jeff Thompson <jefft0@remap.ucla.edu>
2018-11-15 14:02:00 -0800

    generalized-object: Added GeneralizedObjectHandler.

---
+++ include/cnl-cpp/generalized-object/generalized-object-handler.hpp
@@ -0,0 +1,282 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef CNL_CPP_GENERALIZED_OBJECT_HANDLER_HPP
+#define CNL_CPP_GENERALIZED_OBJECT_HANDLER_HPP
+
+#include <cnl-cpp/generalized-object/content-meta-info-object.hpp>
+#include "../segmented-object-handler.hpp"
+
+namespace cnl_cpp {
+
+/**
+ * GeneralizedObjectHandler extends Namespace::Handler and attaches to a
+ * Namespace node to fetch the _meta packet for a generalized object and, if
+ * necessary, assemble the contents of segment packets into a single block of
+ * memory.
+ */
+class GeneralizedObjectHandler : public Namespace::Handler {
+public:
+  typedef ndn::func_lib::function<void
+    (const ndn::ptr_lib::shared_ptr<ContentMetaInfoObject>& contentMetaInfo,
+     ndn::Blob contentBlob)> OnGeneralizedObject;
+
+  /**
+   * Create a GeneralizedObjectHandler with the optional onGeneralizedObject
+   * callback.
+   * @param onGeneralizedObject (optional) When the ContentMetaInfo is received
+   * and the hasSegments is false, this calls 
+   * onGeneralizedObject(contentMetaInfo, other) where contentMetaInfo is the
+   * ContentMetaInfo and other is the "other" info. If the hasSegments flag is
+   * true, when the segments are received and assembled into a single block of
+   * memory, this calls onGeneralizedObject(contentMetaInfo, contentBlob) where
+   * contentMetaInfo is the ContentMetaInfo and contentBlob is the Blob,
+   * assembled from the segment contents. If you don't supply an
+   * onGeneralizedObject callback here, you can call addOnStateChanged on the
+   * Namespace object to which this is attached and listen for the OBJECT_READY
+   * state.
+   */
+  GeneralizedObjectHandler
+    (const OnGeneralizedObject& onGeneralizedObject = OnGeneralizedObject())
+  : impl_(ndn::ptr_lib::make_shared<Impl>(onGeneralizedObject))
+  {
+  }
+
+  /**
+   * Add an onSegment callback. When a new segment is available, this calls
+   * onSegment as described below. Segments are supplied in order.
+   * This is only used if the ContentMetaInfo hasSegments flag is true.
+   * @param onSegment This calls
+   * onSegment(segmentNamespace) where segmentNamespace is the Namespace where
+   * you can use segmentNamespace.getObject(). You must check if
+   * segmentNamespace is null because after supplying the final segment, this
+   * calls onSegment(null) to signal the "end of stream".
+   * NOTE: The library will log any exceptions thrown by this callback, but for
+   * better error handling the callback should catch and properly handle any
+   * exceptions.
+   * @return The callback ID which you can use in removeCallback().
+   */
+  uint64_t
+  addOnSegment(const SegmentedObjectHandler::OnSegment& onSegment)
+  {
+    return impl_->addOnSegment(onSegment);
+  }
+
+  /**
+   * Remove the callback with the given callbackId. This does not search for the
+   * callbackId in child nodes. If the callbackId isn't found, do nothing.
+   * This is only used if the ContentMetaInfo hasSegments flag is true.
+   * @param callbackId The callback ID returned, for example, from
+   * addOnSegment.
+   */
+  void
+  removeCallback(uint64_t callbackId) { impl_->removeCallback(callbackId); }
+
+  /**
+   * Get the number of outstanding interests which this maintains while fetching
+   * segments.
+   * This is only used if the ContentMetaInfo hasSegments flag is true.
+   * @return The Interest pipeline size.
+   */
+  int
+  getInterestPipelineSize() { return impl_->getInterestPipelineSize(); }
+
+  /**
+   * Set the number of outstanding interests which this maintains while fetching
+   * segments.
+   * This is only used if the ContentMetaInfo hasSegments flag is true.
+   * @param interestPipelineSize The Interest pipeline size.
+   * @throws runtime_error if interestPipelineSize is less than 1.
+   */
+  void
+  setInterestPipelineSize(int interestPipelineSize)
+  {
+    impl_->setInterestPipelineSize(interestPipelineSize);
+  }
+
+  /**
+   * Get the initial Interest count (as described in setInitialInterestCount).
+   * This is only used if the ContentMetaInfo hasSegments flag is true.
+   * @return The initial Interest count.
+   */
+  int
+  getInitialInterestCount() { return impl_->getInitialInterestCount(); }
+
+  /**
+   * Set the number of initial Interests to send for segments. By default this
+   * just sends an Interest for the first segment and waits for the response
+   * before fetching more segments, but if you know the number of segments you
+   * can reduce latency by initially requesting more segments. (However, you
+   * should not use a number larger than the Interest pipeline size.)
+   * This is only used if the ContentMetaInfo hasSegments flag is true.
+   * @param initialInterestCount The initial Interest count.
+   * @throws runtime_error if initialInterestCount is less than 1.
+   */
+  void
+  setInitialInterestCount(int initialInterestCount)
+  {
+    impl_->setInitialInterestCount(initialInterestCount);
+  }
+
+  static const ndn::Name::Component&
+  getNAME_COMPONENT_META() { return getValues().NAME_COMPONENT_META; }
+
+  static const ndn::Name::Component&
+  getNAME_COMPONENT_MANIFEST() { return getValues().NAME_COMPONENT_MANIFEST; }
+
+  /**
+   * This is called by Namespace when a packet is received. If this is the
+   * _meta packet, then decode it.
+   */
+  virtual bool
+  canDeserialize
+    (Namespace& objectNamespace, const ndn::Blob& blob,
+     const OnDeserialized& onDeserialized)
+  {
+    return impl_->canDeserialize(objectNamespace, blob, onDeserialized);
+  }
+
+protected:
+  virtual void
+  onNamespaceSet() { impl_->onNamespaceSet(&getNamespace()); }
+
+private:
+  /**
+   * GeneralizedObjectHandler::Impl does the work of GeneralizedObjectHandler.
+   * It is a separate class so that GeneralizedObjectHandler can create an
+   * instance in a shared_ptr to use in callbacks.
+   */
+  class Impl : public ndn::ptr_lib::enable_shared_from_this<Impl> {
+  public:
+    /**
+     * Create a new Impl, which should belong to a shared_ptr.
+     * @param onSegmentedObject See the GeneralizedObjectHandler constructor.
+     */
+    Impl(const OnGeneralizedObject& onGeneralizedObject);
+
+    void
+    onNamespaceSet(Namespace* nameSpace);
+
+    uint64_t
+    addOnSegment(const SegmentStreamHandler::OnSegment& onSegment)
+    {
+      // Pass through to the segmentedObjectHandler_.
+      return segmentedObjectHandler_->addOnSegment(onSegment);
+    }
+
+    void
+    removeCallback(uint64_t callbackId)
+    {
+      // Pass through to the segmentedObjectHandler_.
+      segmentedObjectHandler_->removeCallback(callbackId);
+    }
+
+    int
+    getInterestPipelineSize()
+    {
+      // Pass through to the segmentedObjectHandler_.
+      return segmentedObjectHandler_->getInterestPipelineSize();
+    }
+
+    void
+    setInterestPipelineSize(int interestPipelineSize)
+    {
+      // Pass through to the segmentedObjectHandler_.
+      segmentedObjectHandler_->setInterestPipelineSize(interestPipelineSize);
+    }
+
+    int
+    getInitialInterestCount()
+    {
+      // Pass through to the segmentedObjectHandler_.
+      return segmentedObjectHandler_->getInitialInterestCount();
+    }
+
+    void
+    setInitialInterestCount(int initialInterestCount)
+    {
+      // Pass through to the segmentedObjectHandler_.
+      segmentedObjectHandler_->setInitialInterestCount(initialInterestCount);
+    }
+
+    void
+    setNamespace(Namespace* nameSpace) { namespace_ = nameSpace; }
+
+    bool
+    canDeserialize
+      (Namespace& objectNamespace, const ndn::Blob& blob,
+       const OnDeserialized& onDeserialized);
+
+  private:
+    bool
+    onObjectNeeded
+      (Namespace& nameSpace, Namespace& neededNamespace,
+       uint64_t callbackId);
+
+    /**
+     * This is called when the SegmentedObjectHandler finishes.
+     * @param contentBlob The Blob from the SegmentedObjectHandler.
+     * @param contentMetaInfo The ContentMetaInfoObject from canDeserialize.
+     */
+    void onSegmentedObject
+      (ndn::Blob contentBlob,
+       const ndn::ptr_lib::shared_ptr<ContentMetaInfoObject>& contentMetaInfo);
+
+    ndn::ptr_lib::shared_ptr<SegmentedObjectHandler> segmentedObjectHandler_;
+    OnGeneralizedObject onGeneralizedObject_;
+    Namespace* namespace_;
+  };
+
+  /**
+   * Values holds values used by the static member values_.
+   */
+  class Values {
+  public:
+    Values()
+    : NAME_COMPONENT_META("_meta"),
+      NAME_COMPONENT_MANIFEST("_manifest")
+    {}
+
+    ndn::Name::Component NAME_COMPONENT_META;
+    ndn::Name::Component NAME_COMPONENT_MANIFEST;
+  };
+
+  /**
+   * Get the static Values object, creating it if needed. We do this explicitly
+   * because some C++ environments don't handle static constructors well.
+   * @return The static Values object.
+   */
+  static Values&
+  getValues()
+  {
+    if (!values_)
+      values_ = new Values();
+
+    return *values_;
+  }
+
+  ndn::ptr_lib::shared_ptr<Impl> impl_;
+  static Values* values_;
+};
+
+}
+
+#endif
---
+++ src/generalized-object/generalized-object-handler.cpp
@@ -0,0 +1,121 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <memory.h>
+#include <cnl-cpp/generalized-object/generalized-object-handler.hpp>
+
+using namespace std;
+using namespace ndn;
+using namespace ndntools;
+using namespace ndn::func_lib;
+
+namespace cnl_cpp {
+
+GeneralizedObjectHandler::Impl::Impl(const OnGeneralizedObject& onGeneralizedObject)
+: // Instead of making this inherit from SegmentedObjectHandler, we create one
+  // here and pass the method calls through.
+  segmentedObjectHandler_(ptr_lib::make_shared<SegmentedObjectHandler>()),
+  // We'll call onGeneralizedObject if we don't use the SegmentedObjectHandler.
+  onGeneralizedObject_(onGeneralizedObject), namespace_(0)
+{
+}
+
+void
+GeneralizedObjectHandler::Impl::onNamespaceSet(Namespace* nameSpace)
+{
+  // Store getNamespace() in impl_. We do this instead of keeping a pointer to
+  // this outer Handler object since it might be destroyed.
+  namespace_ = nameSpace;
+
+  namespace_->addOnObjectNeeded
+    (bind(&GeneralizedObjectHandler::Impl::onObjectNeeded, shared_from_this(), _1, _2, _3));
+  // We don't attach the SegmentedObjectHandler until we need it.
+}
+
+bool
+GeneralizedObjectHandler::Impl::onObjectNeeded
+  (Namespace& nameSpace, Namespace& neededNamespace,
+   uint64_t callbackId)
+{
+  if (&neededNamespace != namespace_)
+    // Don't respond for child namespaces (including when we call objectNeeded
+    // on the _meta child below).
+    return false;
+
+  (*namespace_)[getNAME_COMPONENT_META()].objectNeeded();
+  return true;
+}
+
+bool
+GeneralizedObjectHandler::Impl::canDeserialize
+  (Namespace& objectNamespace, const Blob& blob,
+   const OnDeserialized& onDeserialized)
+{
+  if (!(objectNamespace.getName().size() == namespace_->getName().size() + 1 &&
+        objectNamespace.getName()[-1] == getNAME_COMPONENT_META()))
+    // Not the _meta packet. Ignore.
+    return false;
+
+  // Decode the ContentMetaInfo.
+  ptr_lib::shared_ptr<ContentMetaInfoObject> contentMetaInfo =
+    ptr_lib::make_shared<ContentMetaInfoObject>(ContentMetaInfo());
+  // TODO: Report a deserializing error.
+  contentMetaInfo->wireDecode(blob);
+
+  // This will set the object for the _meta Namespace node.
+  onDeserialized(contentMetaInfo);
+
+  if (contentMetaInfo->getHasSegments()) {
+    // Initiate fetching segments. This will call onGeneralizedObject.
+    segmentedObjectHandler_->addOnSegmentedObject
+      (bind(&GeneralizedObjectHandler::Impl::onSegmentedObject,
+       shared_from_this(), _1, contentMetaInfo));
+    segmentedObjectHandler_->setNamespace(namespace_);
+    namespace_->objectNeeded();
+
+    // TODO: Fetch the _manifest packet. How to override per-packet verification?
+  }
+  else {
+    // No segments, so the object is the ContentMetaInfo "other" Blob.
+    if (!contentMetaInfo->getOther().isNull()) {
+      namespace_->setObject(ptr_lib::make_shared<BlobObject>
+        (contentMetaInfo->getOther()));
+
+      if (onGeneralizedObject_)
+        onGeneralizedObject_(contentMetaInfo, contentMetaInfo->getOther());
+    }
+  }
+
+  return true;
+}
+
+void
+GeneralizedObjectHandler::Impl::onSegmentedObject
+  (Blob contentBlob,
+   const ptr_lib::shared_ptr<ContentMetaInfoObject>& contentMetaInfo)
+{
+  if (onGeneralizedObject_)
+    onGeneralizedObject_(contentMetaInfo, contentBlob);
+}
+
+GeneralizedObjectHandler::Values* GeneralizedObjectHandler::values_ = 0;
+
+}
